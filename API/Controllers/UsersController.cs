df72a636ad242fd7f7fb39aa0f4aa2cc4219370c
tiaan <tiaanhugo@protonmail.com>
2025-04-23 11:32:24 +0200

    First Commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController(DataContext context) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            List<AppUser> users = await context.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id:int}")] // api/users/1
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(int id)
+        {
+            AppUser user = await context.Users.FindAsync(id);
+
+            if (user == null) return NotFound();
+            return Ok(user);
+        }
+    }
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace API.Entities;
+
+public class AppUser
+{
+    public required int Id { get; set; }
+    public required string UserName { get; set; }
+}
